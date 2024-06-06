e6a34fbfbb8558fed77439243fd6ca84fb2105f7
Simon Sprouse <simonhsprouse@gmail.com>
2024-06-06 00:04:52 -0500

    Initial Commit

---
+++ TextEditor/TextEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextEditorApp.swift
+//  TextEditor
+//
+//  Created by Simon Sprouse on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TextEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
